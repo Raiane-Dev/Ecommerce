2ba1e34175b74074383ccee4c5ca94b0c37a12c6
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2021-11-21 10:54:27 -0300

    Update

--- config.php
+++ config.php
@@ -12,7 +12,7 @@
 	spl_autoload_register($autoload);
 
 
-	define('INCLUDE_PATH','http://localhost/Curso/Projeto/Ecommerce/');
+	define('INCLUDE_PATH','http://localhost/Ecommerce/');
 	define('INCLUDE_PATH_PAINEL',INCLUDE_PATH.'painel/');
 
 	define('BASE_DIR_PAINEL',__DIR__.'/painel');
